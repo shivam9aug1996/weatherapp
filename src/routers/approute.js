8b033d86f9fc1f2f147cc441ad5c7cd4e115fc46
shivam <shivam9aug1996@gmail.com>
2021-01-18 18:33:39 +0530

    change in weather api

--- src/routers/approute.js
+++ src/routers/approute.js
@@ -78,4 +78,29 @@ router.post("/login", async (req,res)=>{
     }
 })
 
+router.post("/weather", async (req,res)=>{
+    try{
+        
+        
+            fname = req.body.fname;
+           
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${fname}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
+
+            const response = await fetch(url);
+
+            const data = await response.json();
+            const arrData = [data];
+            const weatherDescription=arrData[0].weather[0].description;
+           const weather= weatherDescription.charAt(0).toUpperCase()+weatherDescription.substring(1);
+           res.status(200).render("weather",{
+            LocationCountry : `${arrData[0].name}, ${arrData[0].sys.country}`,
+            temp : `${Math.round(arrData[0].main.temp)}°C , ${weather}`,
+            min :`Min ${Math.round(arrData[0].main.temp_min)} °C | Max ${Math.round(arrData[0].main.temp_max)} °C `
+           })
+    }
+    catch(e){
+        res.status(400).status(e);
+    }
+})
+
 module.exports = router;
