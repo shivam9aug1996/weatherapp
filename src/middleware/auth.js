b40b93debf44463d6c9d7d9a81737cc413c353ad
shivam <shivam9aug1996@gmail.com>
2021-01-29 02:22:41 +0530

    add logout feature

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req,res,next)=>{
     try{
         const token =req.cookies.jwt;
        
-        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
+        const verifyUser = jwt.verify(token, "qwertyuiopasdfghjklzxcvbnmqwertyuiooasd");
         
         const user = await Register.findOne({_id:verifyUser._id});
         
